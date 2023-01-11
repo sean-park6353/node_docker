300ee0aba6a70e6917bf5f513c59850253c5a770
parkji seong <package9@inu.ac.kr>
2023-01-11 23:50:35 +0900

    로깅하는거 추가

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express")
+const logger = require("morgan")
+const fs = require("fs")
 
 app = express()
+app.use(logger("combined", {
+    stream: fs.createWriteStream("./access.log", {flags: 'a'})
+}))
+app.use(logger("dev"))
 
 app.get("/", (req, res)=>{
     console.log(123);
-    res.send("123123")
+    res.send("우와아아")
 })
 
 
-app.listen(3000)
+app.listen(4000,()=>console.log("http://localhost:4000"))
